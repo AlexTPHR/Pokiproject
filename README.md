e9d80da1d2f1167c7f3ec08c4f6a338e0122982d
Alex <alex.chart@turningpointhr.com>
2022-05-12 19:23:35 +0100

    commit tree 3cde00017800dc63d3cf2e951f16b78b6017daee

---
+++ README.md
@@ -0,0 +1,22 @@
+# Phase 1 Poki Project
+
+### Overview
+If you are playing one of the original Pokemon 3 games on Gameboy (read, blue or yellow) or even an emulator on PC and don't like surprises AND want to know exactly what Pokemon you want to catch in your area then this is for you!!!
+
+### Features
+- Searches your current area
+- Creates a list of all the Pokemon encounters for this area
+- Use the Pokemon Search box to reveal a picture
+
+### How To Use
+
+1. Read the local town signs in game to find the area you are in
+2. Copy that name and type it into the first search box with the 🔍 symbol
+3. With the list of Pokemon revealed in that area you can now pick which one to see
+4. Search to remind yourself what they look like in game
+
+### Technologies Used
+This HTML/JS project was created using:
+
+- Vanilla JavaScript
+- Pokemon API used https://pokeapi.co/api/v2/
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,49 @@
-alert('hi');
+document.querySelector('#search').addEventListener("click", getPokemon)
+document.querySelector('#pokemonList').addEventListener("click", getPokemonInfo)
+window.addEventListener("keydown", event => {
+    if (event.key === 'ArrowDown') {
+        document.querySelector("body").style.background = "black";}
+    else {document.querySelector("body").style.background = "white";}
+    });
+
+function populateData (data) {
+    document.querySelector(".pokemonBox").innerHTML = `
+        </div>
+        </div>
+        <div class="pokemonInfo">
+        </div>
+        <h1>Current area selected: ${data.name}</h1>
+        <p>Pokemon encounters for this area: ${data.pokemon_encounters.map(e=> e.pokemon.name).join(", ")}</p>
+        `
+}
+
+function getPokemon(e) {
+    const name = document.querySelector("#pokemonName").value;
+    fetch(`https://pokeapi.co/api/v2/location-area/${name}`)
+    .then(response => response.json())
+    .then(data => {
+        populateData(data);
+        });
+}
+function showPokemon(data){
+    document.querySelector(".pContainer").innerHTML = `
+        </div>
+        </div>
+        <div>
+        <img
+            src=${data.sprites['front_default']}
+            alt=${data.name}
+            />
+        </div>
+        <h1>Pokemon: ${data.name}</h1>
+        `
+}
+
+function getPokemonInfo(e) {
+    const name = document.querySelector("#pokemonSearch").value;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then(response => response.json())
+    .then(data => {
+        showPokemon(data);
+        });
+    }
